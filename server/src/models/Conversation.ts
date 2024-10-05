882da462917b558fe108dfb2b528c4435e6992c0
Florian Mealing <florianmealing@gmail.com>
2024-10-05 14:12:56 +0100

    Made some more changes

--- server/src/models/Conversation.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const ConversationSchema = new mongoose.Schema({
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  createdAt: { type: Date, default: Date.now },
-});
-
-const Conversation = mongoose.model("Conversation", ConversationSchema);
-export default Conversation;
